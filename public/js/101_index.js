87838c29eeda78a53cafbbd01636fb90d50b6b48
田淵健太 <>
2019-12-15 23:00:33 +0900

    トップページに円グラフを追加（DB接続まだ)

---
+++ public/js/101_index.js
@@ -0,0 +1,33 @@
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ["当月消費", "予算残"],
+        datasets: [{
+            label: '今月の変動費',
+            data: [20000,300 ],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+      title:{
+        display:true,
+        text:'今月の変動費'
+      },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
--- resources/views/101_index.blade.php
+++ resources/views/101_index.blade.php
@@ -16,6 +16,9 @@
 <link rel="apple-touch-icon" href="./images/apple-touch-icon.png" sizes="180x180">
 <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon">
 <link rel="icon" type="image/png" href="./images/android-touch-icon.png" sizes="192x192">
+<!--chart.js-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
+
 <title>家計簿</title>
 </head>
 
@@ -65,6 +68,12 @@
           <a href=""></a><!--開発中-->
         </div>
     </div><!--row-->
+    <div class="container col-xs-12 mtpx-50">
+    <canvas id="myChart" class="col-8 offset-2"></canvas>
+    </div><!--row-->
+ 
   </div><!--container-->
+<script src="js/101_index.js"></script>
+
 </body>
 </html>
